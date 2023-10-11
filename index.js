62bcd5b262f46559d0d55793f1225744007e6fe1
Sethy007 <44080277+Sethy007@users.noreply.github.com>
2023-10-12 06:27:58 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+ 
+greet('Sethy', 'Khun');
+
+function square(number) {
+    return number * number;
+}
+
+// console.log(square(9));
+let num = square(4);
+console.log(num);
